8e699ab9ba9e5f47a9953aba22ada3ab4290d185
kcccendana <kc_telyn@yahoo.com>
2023-09-27 00:15:22 +0900

    add index for foreign key

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -37,3 +37,18 @@ CREATE TABLE invoice_items (
   FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
+CREATE TABLE  medical_histories_treatments  (
+   medical_histories_id  int,
+   treatments_id  int,
+  PRIMARY KEY ( medical_histories_id ,  treatments_id ),
+  FOREIGN KEY ( medical_histories_id ) REFERENCES  medical_histories  ( id );
+  FOREIGN KEY ( treatments_id ) REFERENCES  treatments  ( id );
+);
+
+/*INDEXES ON FOREIGN KEY*/
+CREATE INDEX ndx_medhistories_patient_id ON medical_histories(patient_id);
+CREATE INDEX ndx_medhistories_id ON medical_histories(id);
+CREATE INDEX ndx_invoice_items_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX ndx_invoice_items_treatment_id ON invoice_items(treatment_id);
+CREATE INDEX ndx_medhistories_treatments_med_histories_id ON medical_histories_treatments(medical_histories_id);
+CREATE INDEX ndx_medhistories_treatments_treatments_id ON medical_histories_treatments(treatments_id);
